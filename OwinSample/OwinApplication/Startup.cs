2a3b47bede4f844438c2c482c084cd577e69e9e6
Tuan Nguyen <tuan@users.noreply.github.com>
2015-01-13 20:24:13 -0800

    add OwinApplication and Startup class

---
+++ OwinSample/OwinApplication/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace OwinApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
